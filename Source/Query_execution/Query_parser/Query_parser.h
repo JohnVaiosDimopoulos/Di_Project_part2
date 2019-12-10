0a41c744c7f65a8ce9a93fab6a646c7e3dee9153
supergohan <johnvaiosdim@yahoo.com>
2019-12-10 18:54:32 +0200

    outline of the query execution

---
+++ Source/Query_execution/Query_parser/Query_parser.h
@@ -0,0 +1,10 @@
+#ifndef MULTI_JOIN_QUERY_PARSER_H
+#define MULTI_JOIN_QUERY_PARSER_H
+
+#include "../../Query/Query.h"
+
+typedef struct Parsed_Query* Parsed_Query_Ptr;
+
+Parsed_Query_Ptr Parse_Query(Query_Ptr);
+
+#endif //MULTI_JOIN_QUERY_PARSER_H
