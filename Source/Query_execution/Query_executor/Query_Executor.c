fd50fee5353762cc83cd2824f3646b91222ab49b
supergohan <johnvaiosdim@yahoo.com>
2019-12-12 18:44:00 +0200

    Row_id

--- Source/Query_execution/Query_executor/Query_Executor.c
+++ Source/Query_execution/Query_executor/Query_Executor.c
@@ -1,13 +1,6 @@
 #include "Query_Executor.h"
 #include "../Query_parser/Query_parser.h"
 
-
-
-
-
-
-
-
 void Execute_Query(Query_Ptr Query, Table_Ptr Relations){
 
   //1.Parse the query
@@ -15,11 +8,8 @@ void Execute_Query(Query_Ptr Query, Table_Ptr Relations){
   int cnt = Get_Num_of_Relations(Parsed_Query);
   Delete_Parsed_Query(Parsed_Query);
 
-
   //2.do the preparations
-    //1.give row ids
     //2.make the execution tree
-
   //3.execute the query based on the preparation
     //1.input the execution tree + the relations we need to
     //2.get the result as an output
