88eb25c3ceb6fece320e4f689056f0436bfd54d4
supergohan <johnvaiosdim@yahoo.com>
2019-12-16 20:57:55 +0200

    Intermediate result

--- Source/Query_execution/Query_executor/Join_Execution/Join_Execution.c
+++ Source/Query_execution/Query_executor/Join_Execution/Join_Execution.c
@@ -5,12 +5,11 @@
 
 
 
-static void Execute_Self_Join(Join_Ptr Join,Filter_Result_Ptr Filter_Results) {
+static void Execute_Self_Join(Join_Ptr Join,Filter_Result_Ptr Filter_Results,Intermediate_Result_ptr Intermediate_Result) {
 /*
     if(is in the filters result and the filter is not used yet){
         get the row_ids/value for each column based on the filter
     }
-
     else if(is the relation in the indermediate result){
       get the row_ids/value for each column based on the result
     }
@@ -24,17 +23,17 @@ static void Execute_Self_Join(Join_Ptr Join,Filter_Result_Ptr Filter_Results) {
    return result;*/
 }
 
-static int Check_if_relations_already_in_result(Join_Ptr Join){
+static int Check_if_relations_already_in_result(Join_Ptr Join,Intermediate_Result_ptr Intermediate_Result){
 /*    if(Is_in_result(Rel1) && Is_in_result(Rel2))
          return 1;
     return 0*/
 }
 
-static void Execute_Scan_Join(Join_Ptr Join){
+static void Execute_Scan_Join(Join_Ptr Join,Intermediate_Result_ptr old_result){
 /*
  * also input the intermediate result as old_result;
- * Make row_id/value pairs for rel1 based on join column=t1 (rid|v)
- * Make row_id/value paits for rel2 based on join column=t2 (rid|v)
+ * Make row_id/value pairs for rel1 based on join column=  t1(rid|v)
+ * Make row_id/value paits for rel2 based on join column=  t2(rid|v)
  * result new_result;
  * for(int i=0;i<num_of_rows_of_the_result;i++){
  *  row_tuple = old_res[i];
@@ -42,16 +41,13 @@ static void Execute_Scan_Join(Join_Ptr Join){
  *  new_result.insert(row_pair);
  *  }
  * }
-
-
 */
 }
 
 
+void Execute_Joins(Execution_Queue_Ptr Execution_Queue,Filter_Result_Ptr Filter_Results,Table_Ptr Relations){
 
-void Execute_Joins(Execution_Queue_Ptr Execution_Queue,Filter_Result_Ptr Filter_Results){
-
-  // if at any point we get a null result return null
+  Intermediate_Result_ptr Intermediate_Result = NULL;
   Join_Ptr Last_Join = NULL;
   // we will also keep the data of the last join;
 
@@ -63,8 +59,8 @@ void Execute_Joins(Execution_Queue_Ptr Execution_Queue,Filter_Result_Ptr Filter_
       //execute_Self_Join
     }
 
-    else if(Check_if_relations_already_in_result(Current_Join)){
-        Execute_Scan_Join(Current_Join);
+    else if(Check_if_relations_already_in_result(Current_Join,Intermediate_Result)){
+        Execute_Scan_Join(Current_Join,Intermediate_Result);
     }
 
     else if (Is_Same_Column_used(Last_Join,Current_Join)){
--- Source/Query_execution/Query_executor/Join_Execution/Join_Execution.h
+++ Source/Query_execution/Query_executor/Join_Execution/Join_Execution.h
@@ -4,6 +4,28 @@
 #include "../../Execution_Queue/Execution_Queue.h"
 #include "../Filter_Executor/Filter_Executor.h"
 
-void Execute_Joins(Execution_Queue_Ptr Execution_Queue,Filter_Result_Ptr Filter_Results);
+struct Result_Node{
+  uint64_t * row_ids;
+  int relation;
+};
+
+typedef struct Result_Node* Result_Node_Ptr;
+
+struct Intermediate_Result{
+  uint64_t * relations_in_the_result;
+  int num_of_results;
+  int num_of_relations_in_result;
+  Result_Node_Ptr Rows
+};
+
+typedef struct Intermediate_Result* Intermediate_Result_ptr;
+
+
+
+
+
+
+void Execute_Joins(Execution_Queue_Ptr Execution_Queue,Filter_Result_Ptr Filter_Results,Table_Ptr Relations);
+
 
 #endif //MULTI_JOIN_JOIN_EXECUTION_H
