78d51b7e06bfbb0ae0090e03ac80a3d1a83a57b2
Danae Roumelioti <sdi1600145@di.uoa.gr>
2019-12-19 00:03:43 +0200

    Projections

--- Source/Query_execution/Query_executor/Projection_Executor/Projection_Executor.c
+++ Source/Query_execution/Query_executor/Projection_Executor/Projection_Executor.c
@@ -15,7 +15,7 @@ void Execute_Projections(Intermediate_Result_Ptr Res, Parsed_Query_Ptr Parsed_Qu
 
   int num_of_proj = Get_Num_of_Projections(Parsed_Query);
   Projection_Ptr Proj = Get_Projections(Parsed_Query);
-  for(int i = 1; i < num_of_proj; i++) {
+  for(int i = 0; i < num_of_proj; i++) {
 
     Projection_Ptr current_proj = Get_Proj_by_index(Proj, i);
 	int rel = *Get_Projection_Relation(current_proj);
@@ -44,10 +44,17 @@ void Execute_Projections(Intermediate_Result_Ptr Res, Parsed_Query_Ptr Parsed_Qu
 	  if(Res->relations_in_result[rel]) {
         printf("exists -> %d\n", Res->relations_in_result[rel]);
 	    
+        int index;
+        for(int k = 0; k < Res->num_of_relations; k++) {
+          if(Res->row_ids[0][k].relation == rel) {
+		    index = Res->row_ids[0][k].relation;
+		    break;
+		  }
+		}
 	    //copy result_row_ids
 		uint64_t result_row_ids[num_of_res];
         for(int j = 0; j < num_of_res; j++){
-          result_row_ids[j] = row_ids[j][rel].row_id;
+          result_row_ids[j] = row_ids[j][index].row_id;
 //          printf("row_id (intermdt) %d ", result_row_ids[j]);
 	      Tuple_Ptr current_tuple = Get_Shell_Array_by_index(current_shell, col, result_row_ids[j]);
 	      sum += current_tuple->element;
